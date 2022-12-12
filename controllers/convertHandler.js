b2ff9ff1b5366d7cacf4ca84e1279def9f157b3a
Yehuda Lehrfield <yll2113@columbia.edu>
2022-12-12 09:56:01 -0500

    feat: conversion via api

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -20,26 +20,35 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     const numRegex = /^\d+/ //get num from left of string after (optional) digits
-    let result = input.match(numRegex)[0];
+    // get number (valid or not)
+    let num = input.split(/[a-z]/i)[0]
+    console.log(num)
+    // let result = input.match(numRegex)[0];
+    result =  num == '' ? 1 : ( isNaN(num) || num <= 0) ? null : num;
     
     // return null if input number <=0, 1 if no input number, or input number
-    return ((result || 1) <= 0) ? null : (result || 1);
+    // return ((result || 1) <= 0) ? null : (result || 1);
+    return result;
+
   };
   
   this.getUnit = function(input) {
     const unitRegex = /\D+$/ //get unit from right of string after (optional) digits
     let result = input.match(unitRegex)[0]; 
 
-    return (units.hasOwnProperty(result)) ? result : null; // only return valid units
+    // return null if invalid unit 
+    return (units.hasOwnProperty(result)) ? result : null; 
   };
   
   this.getReturnUnit = function(initUnit) {
+    // get corresponding unit
     let result = units[initUnit];
     
     return result;
   };
 
   this.spellOutUnit = function(unit) {
+    // get full unit
     let result = unitsFull[unit];
     
     return result;
@@ -51,6 +60,7 @@ function ConvertHandler() {
     const miToKm = 1.60934;
     let conversionFactor = 0;
 
+    // get appropriate conversion factor
     switch (initUnit.toLowerCase()) {
       case 'gal': 
         conversionFactor = galToL;
@@ -74,13 +84,14 @@ function ConvertHandler() {
         conversionFactor = 1;
     }
     
+    // perform conversion
     let result = initNum*conversionFactor;
     
-    return result;
+    return result.toFixed(5);
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result = `${initNum} ${spellOutUnit(initUnit)} converts to ${returnNum} ${spellOutUnit(returnUnit)} `;
+    let result = `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
      
     return result;
   };
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,25 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
+
+  app.get('/api/convert', (req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    res.json({
+      initNum: initNum,
+      initUnit: initUnit,
+      returnNum: returnNum, 
+      returnUnit: returnUnit,
+      string: string
+    });
+    
+  });
 
 };
