589a666516a4daa9d6a2d060d8e7fe402b56d57b
Yehuda Lehrfield <yll2113@columbia.edu>
2022-12-12 20:10:34 -0500

    feat: valid input checks

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -19,23 +19,32 @@ function ConvertHandler() {
   }
   
   this.getNum = function(input) {
-    const numRegex = /^\d+/ //get num from left of string after (optional) digits
-    // get number (valid or not)
-    let num = input.split(/[a-z]/i)[0]
-    console.log(num)
-    // let result = input.match(numRegex)[0];
-    result =  num == '' ? 1 : ( isNaN(num) || num <= 0) ? null : num;
-    
-    // return null if input number <=0, 1 if no input number, or input number
-    // return ((result || 1) <= 0) ? null : (result || 1);
+    let result;
+    // get raw input number (all chars before alphabetic)
+    let rawNumInput = input.split(/[a-z]/i)[0];
+    // split into array by /'s
+    let numInputArr = rawNumInput.split('/');
+    // Checks: 
+    // 1. more than two elems - invalid (null)
+    // 2. one elem - check if empty (1) or invalid (null)
+    // 3. two elems - check validity of both elems and perform division
+    if (numInputArr.length > 2) {
+      return null
+    } else if (numInputArr.length == 1) {
+      let numCheck = numInputArr[0]
+      result = (numCheck == '') ? 1 : (isNaN(numCheck) || numCheck == '0') ? null : numCheck; 
+    } else {
+      let validNumer = !isNaN(numInputArr[0])
+      let validDenom = !isNaN(numInputArr[1])
+      result = (!validNumer || !validDenom) ? null : (numInputArr[0]/numInputArr[1]).toFixed(5);
+    }
     return result;
-
   };
   
   this.getUnit = function(input) {
     const unitRegex = /\D+$/ //get unit from right of string after (optional) digits
-    let result = input.match(unitRegex)[0]; 
-
+    if (!unitRegex.test(input)) return null;
+    let result = input.match(unitRegex)[0].toLowerCase(); 
     // return null if invalid unit 
     return (units.hasOwnProperty(result)) ? result : null; 
   };
--- routes/api.js
+++ routes/api.js
@@ -11,17 +11,32 @@ module.exports = function (app) {
     let input = req.query.input;
     let initNum = convertHandler.getNum(input);
     let initUnit = convertHandler.getUnit(input);
-    let returnNum = convertHandler.convert(initNum, initUnit);
-    let returnUnit = convertHandler.getReturnUnit(initUnit);
-    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-
-    res.json({
-      initNum: initNum,
-      initUnit: initUnit,
-      returnNum: returnNum, 
-      returnUnit: returnUnit,
-      string: string
-    });
+    let string = ''
+    if (!initNum && !initUnit) {
+      string = 'invalid number and unit'
+      console.log(string)
+      res.send(string)
+    } else if (!initNum) {
+      string = 'invalid number'
+      console.log(string)
+      res.send(string)
+    } else if (!initUnit) {
+      string = 'invalid unit'
+      console.log(string)
+      res.send(string)
+    } else {
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      
+      res.json({
+        initNum: initNum,
+        initUnit: initUnit,
+        returnNum: returnNum, 
+        returnUnit: returnUnit,
+        string: string
+      });
+    }
     
   });
 
